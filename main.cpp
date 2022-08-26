1b1f4a0c2a47b99bdf04655948c235bac59c4d63
Karim Mohamed <s.karim.ibrahim@gmail.com>
2022-08-26 17:56:34 +0200

    implement Constructor and remove destructor

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
+#include "FireForcastingClientBase.h"
 
 using namespace std;
 typedef long long ll;
 
 int main() {
 	// The main function to run the Client class
+	FireForcastingClientBase client;
 	return 0;
 }
